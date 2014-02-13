a6728687a76f42b29213bc6b57fbcce00fa5ff32
Mike Cook <m@mikecook.co.uk>
2014-02-13 12:49:41 +0000

    Move epub building code to DocbookXslWrapper module so we can use as a library, not just command line

--- lib/docbook_xsl_wrapper.rb
+++ lib/docbook_xsl_wrapper.rb
@@ -8,4 +8,19 @@ require "docbook_xsl_wrapper/version"
 
 module DocbookXslWrapper
   GEM_PATH = File.expand_path(File.join('..', '..'), __FILE__)
+
+  def self.build_epub(options)
+    tmp_dir = Dir.mktmpdir
+    options.destination = tmp_dir
+
+    begin
+      puts "Rendering DocBook file #{options.docbook} to #{options.output}\n\n" if options.verbose
+
+      epub = Epub.new(options)
+      epub.create
+    ensure
+      FileUtils.remove_entry_secure tmp_dir
+    end
+  end
+
 end
